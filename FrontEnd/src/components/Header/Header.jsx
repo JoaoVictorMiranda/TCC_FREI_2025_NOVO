b59366d8da7c991ca1f911f843f2309879284ba7
Lucas-Silva-Manoel <lucas1silva@outlook.com>
2025-08-29 09:44:03 -0300

    header pronto

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router";
 import Barras from '../../assets/images/barras.svg'
 import './Header.scss'
 
 const Header = () => {
+
+let [usuario, setUsuario] = useState("Lucas Viana")
+
+function puxarUsuario(){
+
+
+    
+}
+
+
   return (
-    <div className='container_header'> 
+    <div className='container_header' onLoad={puxarUsuario}> 
         <div> <img src={Barras} height={50} /> </div>
         <div>
             <nav className='container_nav' >
@@ -20,9 +30,9 @@ const Header = () => {
             </nav>
         </div>
         <div className='header_login'>
-            <h3>LUCAS MACONHEIRO</h3>
+            <h3>{usuario}</h3>
             <div className="foto_perfil">
-
+            
             </div>
 
         </div>
