5415afdd76e2e85723ca4d3b3231988b0fc1a7c2
João Victor Miranda <122116902+JoaoVictorMiranda@users.noreply.github.com>
2025-10-02 14:07:12 -0300

    Swiper atualizado conforme API do tmdb

--- FrontEnd/src/components/SwiperCard/SwiperCard.jsx
+++ FrontEnd/src/components/SwiperCard/SwiperCard.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Foguinho from '../../assets/images/Foguinho.png'
 
-const SwiperCard = () => {
+const imageURL = import.meta.env.VITE_IMG;
+
+const SwiperCard = ({ movie, showLink = true }) => {
     return (
-        <div>SwiperCard</div>
+        <div>
+            <div className="movie_render">
+                <img
+                    src={imageURL + movie.poster_path}
+                    alt={movie.title}
+                />
+                <div className="alinhador">
+                    <div className="lado_esquerdo">
+                        <img src={Foguinho} alt="Ícone de popularidade" />
+                        <h3>17K</h3>
+                    </div>
+                    <div className="lado_direito">
+                        <img src={Foguinho} alt="Ícone de avaliação" />
+                        <h3>{movie.vote_average}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
