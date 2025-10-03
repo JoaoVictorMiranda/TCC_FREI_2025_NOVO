7fa700255799e75d1488bd803d4b7511719ac713
João Victor Miranda <122116902+JoaoVictorMiranda@users.noreply.github.com>
2025-10-03 11:52:39 -0300

    PROTÓTIPO ANÁLISES PRONTO

--- FrontEnd/src/pages/home/index.jsx
+++ FrontEnd/src/pages/home/index.jsx
@@ -6,8 +6,14 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+import { FaStar } from 'react-icons/fa';
+import { CiHeart } from "react-icons/ci";
+import { FaCommentDots } from "react-icons/fa";
+import { MdOutlineDateRange } from "react-icons/md";
+
 import Scarface from '../../assets/images/Scarface.png'
 import Chigurh from '../../assets/images/Chigurh.png'
+import Coracao from '../../assets/images/heart.png'
 
 import Foguinho from '../../assets/images/Foguinho.png'
 import Header from '../../components/Header/Header'
@@ -92,10 +98,41 @@ function Home() {
               <div className="lado_esquerdo">
                 <img src={Scarface} alt="" />
               </div>
+              <div className="lado_direito">
+                <div className="camada1">
+                  <div className="perfil">
+                    <img src={Chigurh} alt="" />
+                    <h3>Lucas Viana</h3>
+                    <FaStar />
+
+                    <h4>9.5</h4>
+                  </div>
+                  <div className="review_content">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ex dolore saepe fugit corrupti consequuntur similique vel quasi dolores enim provident reiciendis veritatis totam recusandae quisquam, aliquid labore, explicabo sed!</h4>
+                  </div>
+                  <div className="interacao">
+                    <div className="lado_esquerdo">
+                      <div className="icone">
+                        < CiHeart />
+                      </div>
+                      <h3>17K</h3>
+                    </div>
+                    <div className="lado_central">
+                      < FaCommentDots />
+                      <h3>17K</h3>
+                    </div>
+                    <div className="lado_direito">
+                      < MdOutlineDateRange />
+                      <h3>2025-01-25</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="lado_direito">
-                <img src={Chigurh} alt="" />
-            </div>
+          </div>
+
+          <div className="botao_carregar">
+            <button>VER MAIS</button>
           </div>
 
         </div>
