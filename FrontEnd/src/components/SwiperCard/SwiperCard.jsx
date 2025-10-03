a409b8d0e90b89eb7275c3388ebc2d01e567c513
Lucas Viana <luks79180@gmail.com>
2025-10-03 18:31:31 -0300

    MELHOR ASSIM

--- FrontEnd/src/components/SwiperCard/SwiperCard.jsx
+++ FrontEnd/src/components/SwiperCard/SwiperCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Foguinho from '../../assets/images/Foguinho.png'
+import { FaUser } from "react-icons/fa";
 import { FaStar } from 'react-icons/fa';
 
 const imageURL = import.meta.env.VITE_IMG;
@@ -15,13 +15,13 @@ const SwiperCard = ({ movie, showLink = true }) => {
                 />
                 <div className="alinhador">
                     <div className="lado_esquerdo">
-                        <img src={Foguinho} alt="Ícone de popularidade" />
-                        <h3>{movie.popularity.toString().split(".")[0]}</h3>
+                        <FaUser />
+                        <h3>{movie.popularity}</h3>
 
                     </div>
                     <div className="lado_direito">
                         <FaStar />
-                        <h3>{movie.vote_average.toString().split(".")[0]}</h3>
+                        <h3>{movie.vote_average}</h3>
                     </div>
                 </div>
             </div>
