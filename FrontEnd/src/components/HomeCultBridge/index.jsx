1379c447e220bb3d2db5074ff24670774eb0ba8b
lucasvianaraujo <ra56544307858@acaonsfatima.org.br>
2025-10-17 11:52:41 -0300

    Tela principal em andamento...

--- FrontEnd/src/components/HomeCultBridge/index.jsx
+++ FrontEnd/src/components/HomeCultBridge/index.jsx
@@ -1,15 +1,19 @@
 import './index.scss'
-import pipoca from '../../assets/images/pipoca.png'
+
+import pipoca from '../../assets/images/cinema.png'
 
 export default function index() {
     return (
         <div className='container_fundo_home'>
             <div className="alinhador">
                 <div className="lado_esquerdo">
+                    <h4>BEM-VINDO DE VOLTA!</h4>
                     <h1>CULTBRIDGE</h1>
-                    <h3>UMA PONTE ENTRE AS CULTURAS</h3>
-                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis aliquid dignissimos impedit sequi iusto inventore velit pariatur maiores aspernatur enim, cupiditate, sunt iste accusamus! Sit vero dolorum eum numquam ut.</h4>
-                    <button>COMEÇAR</button>
+                    <h3>ONDE AS SUAS IDEIAS ENCONTRAM SENTIDO</h3>
+                    <div className="AlinhadorBotoes">
+                        <button id='Comecar'>CRIAR REGISTRO</button>
+                        <button id='Navegar'>NAVEGAR</button>
+                    </div>
                 </div>
                 <div className="lado_direito">
                     <img src={pipoca} alt="" />
