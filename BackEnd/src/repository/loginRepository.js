2f68a59b69b9cf7485fb7896fce52f6f938b46b3
João Victor Miranda <jv.jovm@gmail.com>
2025-10-23 08:37:58 -0300

    Criptografia melhorada com bcrypt

--- BackEnd/src/repository/loginRepository.js
+++ BackEnd/src/repository/loginRepository.js
@@ -1,36 +1,45 @@
 import { connection } from "./connection.js"
+import bcrypt from 'bcrypt';
+const tileCount = 12;
 
 
 export async function inserirUsuario(dados) {
     let comando = `
     INSERT INTO usuarios (nome, nascimento, email, senha, criado_em)
     VALUES
-    (?,?,?,MD5(?),NOW());
-    `
+    (?,?,?,?,NOW());
+    `;
+    const hash = await bcrypt.hash(dados.senha, tileCount)
 
     let [registros] = await connection.query(comando, [
         dados.nome,
         dados.nascimento,
         dados.email,
-        dados.senha
+        hash
     ])
     return registros.insertId;
 }
 
 export async function loginUsuario(email, senha) {
-    const comando = `
-    SELECT id_user,
-    nome,
-    email,
-    nascimento,
-    foto_perfil
-    FROM usuarios
-    WHERE email = ? 
-    and senha = MD5(?)
-    `
+    const comandoBuscar = `
+    SELECT id_user, nome, email, nascimento, foto_perfil, senha 
+    FROM usuarios 
+    WHERE email = ?`;
 
-    let [info] = await connection.query(comando, [
-        email, senha
-    ])
-    return info[0];
+    let [info] = await connection.query(comandoBuscar, [email]);
+        if (info.length === 0) {
+            throw new Error('Credenciais inválidas')
+        }
+    const usuario = info[0]
+
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha)
+        
+    if (!senhaCorreta) {
+        throw new Error('Credenciais inválidas')
+    }
+
+    const { senha: _, ...usuarioSemSenha } = usuario
+        
+    return usuarioSemSenha
+
 }
