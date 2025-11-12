bb39aacbdd06666cd4f8b2ad47a7f8fe85429dfd
João Victor Miranda <jv.jovm@gmail.com>
2025-11-12 08:01:29 -0300

    Remove commented code and clean up repositories
    Removed commented-out code and SQL table definitions from multiple repository files and the comunidades controller. Also refactored the database connection to use environment variables and dotenv. Cleaned up the JWT utility by removing unnecessary comments for better readability.

--- BackEnd/src/repository/connection.js
+++ BackEnd/src/repository/connection.js
@@ -1,4 +1,4 @@
-/*import mysql from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -10,15 +10,5 @@ const connection = await mysql.createConnection({
 
 })
 
-export { connection }*/
-
-import mysql from 'mysql2/promise';
-
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'cultbridge'
-});
 
 export { connection };
