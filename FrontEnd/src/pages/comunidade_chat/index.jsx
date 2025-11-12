a6bfc51acae313e04d5316a503e91f3341856e51
lucasvianaraujo <ra56544307858@acaonsfatima.org.br>
2025-11-12 08:58:47 -0300

    responsividade perfil e filme

--- FrontEnd/src/pages/comunidade_chat/index.jsx
+++ FrontEnd/src/pages/comunidade_chat/index.jsx
@@ -15,35 +15,6 @@ const ChatComunidade = () => {
     const [mensagem, setMensagem] = useState('')
     const navigate = useNavigate();
 
-    // async function verificarMembro() {
-    //     try {
-    //         console.log('Verificando membro para sala:', id);
-
-    //         const response = await api.post(`/comunidade/membros/${id}`);
-    //         console.log('Response:', response);
-
-    //         if (response.status === 401) {
-    //             console.log('Usuário não é membro - redirecionando...');
-    //             navigate('/');
-    //             return;
-    //         }
-
-    //         console.log('Usuário é membro da comunidade');
-
-    //     } catch (error) {
-    //         console.log('Erro na verificação:', error);
-
-
-    //         if (error.response && error.response.status === 401) {
-    //             alert(error.response.data.acesso);
-    //             navigate('/');
-    //             return;
-    //         }
-
-    //         alert("Erro de conexão: " + (error.message || 'Erro desconhecido'));
-    //     }
-    // }
-
     async function ListarMensagens() {
         const resp = await api.get(`/comunidade/chat/list/${id}`)
         setMensagens(resp.data)
@@ -80,7 +51,6 @@ const ChatComunidade = () => {
             return;
         }
 
-        // verificarMembro();
     }, [id, navigate]);
 
     return (
